fa2101533151d768a9cc4c493298aded543c3a34
Povshy <vladcarrib@gmail.com>
2024-07-18 23:12:44 +0300

    555

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,9 @@ import styles from "./styles.module.css";
 const Header = () => {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>NEWS REACTIFY</h1>
+      <h1 className={styles.title}>
+        NEWS REACTIFY <span style={{ color: "#1abfd5" }}>by Vlad Povshy</span>
+      </h1>
       <p className={styles.date}>{formatDate(new Date())}</p>
     </header>
   );
